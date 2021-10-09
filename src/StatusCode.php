b185b74ba177d0288a75a0bb43972101d0ce04ce
Pierre-Henry Soria <hi@ph7.me>
2021-10-09 12:58:03 +1030

    First main commit
    Containing all status codes and their corresponding tests

---
+++ src/StatusCode.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @author      Pierre-Henry Soria <hi@ph7.me>
+ * @copyright   (c) 2021, Pierre-Henry Soria
+ * @license     MIT License; <https://opensource.org/licenses/MIT>
+ */
+
+namespace PH7\JustHttp;
+
+final class StatusCode
+{
+    /**
+     * Informational 1xx
+     */
+    public const H_CONTINUE = 100; // `CONTINUE` is a reserved keyword
+    public const SWITCHING_PROTOCOLS = 101;
+    public const PROCESSING = 102;
+    public const EARLY_HINTS = 103;
+
+    /**
+     * Successful 2xx
+     */
+    public const OK = 200;
+    public const CREATED = 201;
+    public const ACCEPTED = 202;
+    public const NONAUTHORITATIVE_INFORMATION = 203;
+    public const NO_CONTENT = 204;
+    public const RESET_CONTENT = 205;
+    public const PARTIAL_CONTENT = 206;
+    public const MULTI_STATUS = 207;
+    public const ALREADY_REPORTED = 208;
+    public const IM_USED = 226;
+
+    /**
+     * Redirection 3xx
+     */
+    public const MULTIPLE_CHOICES = 300;
+    public const MOVED_PERMANENTLY = 301;
+    public const FOUND = 302;
+    public const SEE_OTHER = 303;
+    public const NOT_MODIFIED = 304;
+    public const USE_PROXY = 305;
+    public const UNUSED = 306;
+    public const TEMPORARY_REDIRECT = 307;
+
+    /**
+     * Client Error 4xx
+     */
+    public const BAD_REQUEST = 400;
+    public const UNAUTHORIZED = 401;
+    public const PAYMENT_REQUIRED = 402;
+    public const FORBIDDEN = 403;
+    public const NOT_FOUND = 404;
+    public const METHOD_NOT_ALLOWED = 405;
+    public const NOT_ACCEPTABLE = 406;
+    public const PROXY_AUTHENTICATION_REQUIRED = 407;
+    public const REQUEST_TIMEOUT = 408;
+    public const CONFLICT = 409;
+    public const GONE = 410;
+    public const LENGTH_REQUIRED = 411;
+    public const PRECONDITION_FAILED = 412;
+    public const REQUEST_ENTITY_TOO_LARGE = 413;
+    public const REQUEST_URI_TOO_LONG = 414;
+    public const UNSUPPORTED_MEDIA_TYPE = 415;
+    public const REQUESTED_RANGE_NOT_SATISFIABLE = 416;
+    public const EXPECTATION_FAILED = 417;
+    public const TOO_MANY_REQUESTS = 429;
+
+    /**
+     * Server Error 5xx
+     */
+    public const INTERNAL_SERVER_ERROR = 500;
+    public const NOT_IMPLEMENTED = 501;
+    public const BAD_GATEWAY = 502;
+    public const SERVICE_UNAVAILABLE = 503;
+    public const GATEWAY_TIMEOUT = 504;
+    public const VERSION_NOT_SUPPORTED = 505;
+}
---
+++ tests/Unit/StatusCodeTest.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @author      Pierre-Henry Soria <hi@ph7.me>
+ * @copyright   (c) 2021, Pierre-Henry Soria
+ * @license     MIT License; <https://opensource.org/licenses/MIT>
+ */
+
+declare(strict_types=1);
+
+namespace PH7\JustHttp\Test\Unit;
+
+use PH7\JustHttp\StatusCode;
+use PHPUnit\Framework\TestCase;
+
+class HttpCodesTest extends TestCase
+{
+    /**
+     * @dataProvider statusCodesProvider
+     */
+    public function testCorrectStatusCodes(int $expectedCode, int $actualCode): void
+    {
+        $this->assertSame($expectedCode, $actualCode);
+    }
+
+    public function statusCodesProvider(): array
+    {
+        return [
+            // 1xx informational response
+            [100, StatusCode::H_CONTINUE],
+            [101, StatusCode::SWITCHING_PROTOCOLS],
+            [102, StatusCode::PROCESSING],
+            [103, StatusCode::EARLY_HINTS],
+
+            // 2xx success
+            [200, StatusCode::OK],
+            [201, StatusCode::CREATED],
+            [202, StatusCode::ACCEPTED],
+            [203, StatusCode::NONAUTHORITATIVE_INFORMATION],
+            [204, StatusCode::NO_CONTENT],
+            [205, StatusCode::RESET_CONTENT],
+            [206, StatusCode::PARTIAL_CONTENT],
+            [207, StatusCode::MULTI_STATUS],
+            [208, StatusCode::ALREADY_REPORTED],
+            [226, StatusCode::IM_USED],
+
+            // 3xx redirection
+            [300, StatusCode::MULTIPLE_CHOICES],
+            [301, StatusCode::MOVED_PERMANENTLY],
+            [302, StatusCode::FOUND],
+            [303, StatusCode::SEE_OTHER],
+            [304, StatusCode::NOT_MODIFIED],
+            [305, StatusCode::USE_PROXY],
+            [306, StatusCode::UNUSED],
+            [307, StatusCode::TEMPORARY_REDIRECT],
+
+            // 4xx client errors
+            [400, StatusCode::BAD_REQUEST],
+            [401, StatusCode::UNAUTHORIZED],
+            [402, StatusCode::PAYMENT_REQUIRED],
+            [403, StatusCode::FORBIDDEN],
+            [404, StatusCode::NOT_FOUND],
+            [405, StatusCode::METHOD_NOT_ALLOWED],
+            [406, StatusCode::NOT_ACCEPTABLE],
+            [407, StatusCode::PROXY_AUTHENTICATION_REQUIRED],
+            [408, StatusCode::REQUEST_TIMEOUT],
+            [409, StatusCode::CONFLICT],
+            [410, StatusCode::GONE],
+            [411, StatusCode::LENGTH_REQUIRED],
+            [412, StatusCode::PRECONDITION_FAILED],
+            [413, StatusCode::REQUEST_ENTITY_TOO_LARGE],
+            [414, StatusCode::REQUEST_URI_TOO_LONG],
+            [415, StatusCode::UNSUPPORTED_MEDIA_TYPE],
+            [416, StatusCode::REQUESTED_RANGE_NOT_SATISFIABLE],
+            [417, StatusCode::EXPECTATION_FAILED],
+            [429, StatusCode::TOO_MANY_REQUESTS],
+
+            // 5xx server errors
+            [500, StatusCode::INTERNAL_SERVER_ERROR],
+            [501, StatusCode::NOT_IMPLEMENTED],
+            [502, StatusCode::BAD_GATEWAY],
+            [503, StatusCode::SERVICE_UNAVAILABLE],
+            [504, StatusCode::GATEWAY_TIMEOUT],
+            [505, StatusCode::VERSION_NOT_SUPPORTED],
+        ];
+    }
+}
